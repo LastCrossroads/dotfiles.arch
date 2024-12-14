3791e5ec6a63a0b1234a6ad3cf09005bb0143d5a
Last Crossroads <lastcrossroads@proton.me>
2024-12-14 15:22:36 -0500

    packagek updates

--- Documents/fresh_install/fresh_install/packages-hyprland.sh
+++ Documents/fresh_install/fresh_install/packages-hyprland.sh
@@ -1,1 +1,3 @@
-yay -Sy bibata-cursor-theme blueman brightnessctl cliphist dunst grimblast hyprcursor hyprgui hypridle hyprland hyprland-protocols hyprlock hyprpaper hyprpicker hyprshade hyprsunset network-manager-applet nm-connection-manager nwg-look papirus-icon-theme-git papirus-folders playerctl python-pywal16 rofi-wayland qt6ct-kde waybar waypaper wl-clipboard wlroot wshowkeys
+#!/usr/bin/env bash
+
+yay -Sy bibata-cursor-theme blueman brightnessctl cliphist dunst grimblast hyprcursor hyprgui hypridle hyprland hyprland-protocols hyprlock hyprpaper hyprpicker hyprshade hyprsunset network-manager-applet nm-connection-manager nwg-look papirus-icon-theme papirus-folders playerctl python-pywal16 rofi-wayland qt6ct-kde waybar waypaper wl-clipboard wlroot wshowkeys
