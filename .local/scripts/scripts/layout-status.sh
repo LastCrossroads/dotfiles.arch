50237fd70952299941dee862c2cadf3f44213c54
Last Crossroads <lastcrossroads@proton.me>
2024-12-14 15:30:23 -0500

    packagek updates

--- .local/scripts/scripts/layout-status.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-layout="$(bat /etc/vconsole.conf | awk -F'=' '{print toupper($2)}')"
-printf " %s " "$layout"
--- Documents/fresh_install/packages-hyprland.sh
+++ Documents/fresh_install/packages-hyprland.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-yay -Sy bibata-cursor-theme blueman brightnessctl cliphist dateutils dunst grimblast hyprcursor-giti hyprgui hypridle-git hyprland hyprland-protocols-git hyprlock-git hyprpaper-git hyprpicker-git hyprshade-git hyprsunset-git network-manager-applet nm-connection-manager nwg-look papirus-icon-theme-git papirus-folders-git playerctl python-pywal16 rofi-wayland qt6ct-kde waybar waypaper wl-clipboard wleave-git wlroot-git wshowkeys-git wttrbar
+yay -Sy bibata-cursor-theme blueman brightnessctl cliphist dateutils dunst grimblast hyprcursor hyprgui hypridle hyprland hyprland-protocols hyprlock hyprpaper hyprpicker hyprshade hyprsunset network-manager-applet nm-connection-manager nwg-look papirus-icon-theme papirus-folders playerctl python-pywal16 rofi-wayland qt6ct-kde waybar waypaper wl-clipboard wleave wlroot wshowkeys wttrbar
