dc3a9070dc72fdd1aee0660d64e04b9eafc62ae1
LastCrossroads <lastcrossroads@proton.me>
2025-05-26 11:39:42 -0400

    add A/V tools

--- Documents/fresh_install/packages-hyprland.sh
+++ Documents/fresh_install/packages-hyprland.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-paru -Sy bibata-cursor-theme blueman brightnessctl cliphist dateutils dunst grimblast hyprcursor hypridle hyprland hyprland-protocols hyprlock hyprpaper hyprpicker hyprshade hyprsunset network-manager-applet nwg-look papirus-icon-theme papirus-folders playerctl python-pywal16 rofi-wayland qt5-graphicaleffects qt5-quickcontrols2 qt6ct-kde swaync swaync-widgets-git waybar waypaper wl-clipboard wttrbar
+paru -Sy audacity bibata-cursor-theme blueman brightnessctl cliphist dateutils dunst grimblast hyprcursor hypridle hyprland hyprland-protocols hyprlock hyprpaper hyprpicker hyprshade hyprsunset kdenlive network-manager-applet nwg-look obs-vkcapture papirus-icon-theme papirus-folders playerctl python-pywal16 rofi-wayland qt5-graphicaleffects qt5-quickcontrols2 qt6ct-kde swaync swaync-widgets-git waybar waypaper wl-clipboard wttrbar
