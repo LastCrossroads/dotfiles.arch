bbe1aff14c4ae8442a32b4a12fd52ad7aee94d64
LastCrossroads <lastcrossroads@proton.me>
2025-07-28 19:02:12 -0400

    added uwsm

--- Documents/fresh_install/packages-hyprland.sh
+++ Documents/fresh_install/packages-hyprland.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-paru -Sy audacity bibata-cursor-theme blueman brightnessctl cliphist dateutils dunst grimblast hyprcursor hypridle hyprland hyprland-protocols hyprlock hyprpaper hyprpicker hyprshade hyprsunset kdenlive network-manager-applet nwg-look obs-vkcapture papirus-icon-theme papirus-folders playerctl pyprland python-pywal16 rofi-wayland qt5-graphicaleffects qt5-quickcontrols2 qt6ct-kde swaync swaync-widgets-git swww waybar waypaper wl-clipboard wlogout wttrbar xmlstartlet
+paru -Sy audacity bibata-cursor-theme blueman brightnessctl cliphist dateutils dunst grimblast hyprcursor hypridle hyprland hyprland-protocols hyprlock hyprpaper hyprpicker hyprshade hyprsunset kdenlive network-manager-applet nwg-look obs-vkcapture papirus-icon-theme papirus-folders playerctl pyprland python-pywal16 rofi-wayland qt5-graphicaleffects qt5-quickcontrols2 qt6ct-kde swaync swaync-widgets-git swww uwsm waybar waypaper wl-clipboard wlogout wttrbar xmlstartlet
