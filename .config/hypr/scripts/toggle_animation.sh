624ef4dd99b4b70c0c8a13d324419ba90f3c4c62
Last Crossroads <lastcrossroads@proton.me>
2025-09-25 13:40:25 -0400

    notification alerts

--- .config/hypr/scripts/toggle_animation.sh
+++ .config/hypr/scripts/toggle_animation.sh
@@ -3,8 +3,8 @@
 on=$(hyprctl -j getoption animations:enabled | jq --raw-output '.int')
 if [[ $on -eq 1 ]]; then
   hyprctl keyword animations:enabled 0
-  notify-send "Animations off"
+  notify-send "   Animations Disabled"
   else
   hyprctl keyword animations:enabled 1
-  notify-send "Animations on"
+  notify-send "   Animations Enabled"
 fi
--- .config/hypr/scripts/toggle_layout.sh
+++ .config/hypr/scripts/toggle_layout.sh
@@ -4,6 +4,8 @@ current_layout=$(hyprctl -j getoption general:layout | jq -r .str)
 
 if [ "$current_layout" = "dwindle" ]; then
   hyprctl keyword general:layout "scrolling"
+  notify-send "   Layout: Scrolling"
 elif [ "$current_layout" = "scrolling" ]; then
   hyprctl keyword general:layout "dwindle"
+  notify-send "   Layout: Dwindle"
 fi
